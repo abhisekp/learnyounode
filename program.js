d805c526cf4717b13070045d89bcdf13ce4bdd8d
Abhisek Pattnaik <abhisekp@engineer.com>
2016-04-27 00:31:35 +0530

    Sync file read

--- program.js
+++ program.js
@@ -1,3 +1,26 @@
-const sum = (a, b) => a + b
+const fs = require('fs')
 
-console.log(process.argv.slice(2).map(Number).reduce(sum))
+const countNewlines = (fileStr) => {
+  if(!fileStr) {
+    return 0
+  }
+
+  const newlines = fileStr.match(/\n/g)
+
+  if(!newlines) {
+    return 0
+  }
+
+  return newlines.length
+}
+
+
+const getFile = () => {
+  try {
+    const fileStr = fs.readFileSync(process.argv[2], 'utf8')
+    return fileStr
+  } catch (e) {}
+}
+
+const newlines = countNewlines(getFile())
+console.log(newlines)
