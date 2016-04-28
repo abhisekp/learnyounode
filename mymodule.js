6015a688b699e55fba9c76ed70393cfcd80b415d
Abhisek Pattnaik <abhisekp@engineer.com>
2016-04-28 15:03:05 +0530

    Streaming data without using any 3rd party module

--- mymodule.js
+++ mymodule.js
@@ -1,12 +1,17 @@
 const http = require('http')
 const bl = new require('bl')
 
-const processResponse = (cb, res) => {
-  res.pipe(BL((err, data)=> cb(String(data))))
+const processResponse = (cb, data) => {
+  cb(data)
 }
 
 const getData = (url, cb) => {
-  http.get(url, res => processResponse(cb, res))
+  let data = ''
+  http.get(url, res => {
+    res.setEncoding('utf8')
+    res.on('data', (_data) => data += _data)
+    res.on('end', () => processResponse(cb, data))
+  })
 }
 
 module.exports = getData
