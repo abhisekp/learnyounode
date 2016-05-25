3b2b6b3fa51e9a9b8ec70aece813f95fa7ad03a9
Abhisek Pattnaik <abhisekp@engineer.com>
2016-05-25 23:59:06 +0530

    Create TCP connect - emit date

--- program.js
+++ program.js
@@ -1,9 +1,28 @@
 #!/usr/bin/env node
 
-const getData = require('./mymodule');
+const net = require('net');
+const fp = require('lodash/fp');
 
-const urls = process.argv.slice(2);
+const PORT = process.argv[2];
 
-getData(urls, data => {
-  console.log(data);
-});
+const getFormattedDate = dateObj => {
+  const year = dateObj.getFullYear();
+  const month = dateObj.getMonth() + 1;
+  const date = dateObj.getDate();
+  const hours = dateObj.getHours();
+  const minutes = dateObj.getMinutes(); 
+  
+  const pad2 = fp.padCharsStart(0)(2)
+  const monthStr = pad2(month);
+  const dateStr = pad2(date);
+  const hoursStr = pad2(hours);
+  const minutesStr = pad2(minutes);
+  
+  return `${ year }-${ monthStr }-${ dateStr } ${ hoursStr }:${ minutesStr }`
+}
+
+net.createServer(socket => {
+  const data = getFormattedDate(new Date);
+  socket.write(data);
+  socket.end('\n');
+}).listen(PORT);
