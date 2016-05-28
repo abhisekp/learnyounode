512710f17a0c14cca497c9573a70e87cdff03518
Abhisek Pattnaik <abhisekp@engineer.com>
2016-05-28 12:48:29 +0530

    Create Time server using strftime

--- program.js
+++ program.js
@@ -2,25 +2,14 @@
 
 const net = require('net');
 const fp = require('lodash/fp');
+const strftime = require('strftime');
 const parseArgs = require('minimist');
 
 const args = parseArgs(process.argv);
 const PORT = args._[2];
 
 const getFormattedDate = dateObj => {
-  const year = dateObj.getFullYear();
-  const month = dateObj.getMonth() + 1;
-  const date = dateObj.getDate();
-  const hours = dateObj.getHours();
-  const minutes = dateObj.getMinutes();
-
-  const pad2 = fp.padCharsStart(0)(2)
-  const monthStr = pad2(month);
-  const dateStr = pad2(date);
-  const hoursStr = pad2(hours);
-  const minutesStr = pad2(minutes);
-
-  return `${ year }-${ monthStr }-${ dateStr } ${ hoursStr }:${ minutesStr }`
+  return strftime('%Y-%m-%d %H:%M', dateObj);
 }
 
 net.createServer(socket => {
